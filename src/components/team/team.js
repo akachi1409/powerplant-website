d6817ab23e57887c7a9499ff1952f4635925a2d3
trusteddev1325 <trusteddev1325@gmail.com>
2021-12-10 08:59:54 +0900

    fix team member

--- src/components/team/team.js
+++ src/components/team/team.js
@@ -20,28 +20,28 @@ class Team extends React.Component {
             teams: [
                 {
                     image: "https://github.com/powerplantsnft/website/blob/main/pp1.jpeg?raw=true",
-                    title: 'Founder',
-                    name: "Muhani"
+                    title: 'Tony Stark without the billions. On the rocks type of guy, chases with green juice for balance.',
+                    name: "Muhani, Founder"
                 },
                 {
                     image: "https://github.com/powerplantsnft/website/blob/main/pp2.jpeg?raw=true",
-                    title: 'Founder',
-                    name: "Thomas"
+                    title: 'Self-proclaimed crypto-genius. Jack of all trades. Lover of all things nature.',
+                    name: "Thomas, Founder"
                 },
                 {
                     image: "https://github.com/powerplantsnft/website/blob/main/pp3.jpeg?raw=true",
-                    title: 'Artist',
-                    name: "Alexander"
+                    title: 'Scared of heights but would love to go to space.',
+                    name: "Alexander, Artist"
                 },
                 {
                     image: "https://github.com/powerplantsnft/website/blob/main/pp4.jpeg?raw=true",
-                    title: 'Developer',
-                    name: "Joseph"
+                    title: 'Firm believer that the earth is flat. Avoids cliffs.',
+                    name: "JosEmily, Social Media/Outreach"
                 }, 
                 {
                     image: "https://github.com/powerplantsnft/website/blob/main/pp5.jpeg?raw=true",
-                    title: 'Social Media',
-                    name: "Emily"
+                    title: 'Developer by day, master chef by night.',
+                    name: "Joseph, Developer"
                 }
             ]
         }
@@ -53,19 +53,44 @@ class Team extends React.Component {
                 <Container>
                     <header style={{fontStyle: "normal", color:"white"}}> Team Members </header>
                     <Row>
-                        {
-                            this.state.teams.map((item, index) => {
-                                return (
-                                    <Col style={{justifyContent: "center", textAlign: "center"}} key={index}>
-                                        <TeamMember key={index}
-                                            imageUrl={item.image}
-                                            title={item.title}
-                                            name={item.name} />
-                                    </Col>
-                                );
-                            })
-                        }
-                    </Row>
+                        <Col className="team-control-card" md={6} xs={12} >
+                            <TeamMember
+                                imageUrl={this.state.teams[0].image}
+                                title={this.state.teams[0].title}
+                                name={this.state.teams[0].name}
+                            />
+                        </Col>
+                        <Col className="" md={6} xs={12} >
+                            <TeamMember
+                                imageUrl={this.state.teams[1].image}
+                                title={this.state.teams[1].title}
+                                name={this.state.teams[1].name}
+                            />
+                        </Col>
+                    </Row> 
+                    <Row>
+                        <Col className="team-control-card" md={4} xs={12} >
+                            <TeamMember
+                                imageUrl={this.state.teams[2].image}
+                                title={this.state.teams[2].title}
+                                name={this.state.teams[2].name}
+                            />
+                        </Col>
+                        <Col className="team-control-card" md={4} xs={12} >
+                            <TeamMember
+                                imageUrl={this.state.teams[3].image}
+                                title={this.state.teams[3].title}
+                                name={this.state.teams[3].name}
+                            />
+                        </Col>
+                        <Col className="" md={4} xs={12} >
+                            <TeamMember
+                                imageUrl={this.state.teams[4].image}
+                                title={this.state.teams[4].title}
+                                name={this.state.teams[4].name}
+                            />
+                        </Col>
+                    </Row> 
                 </Container>
             </div>
         );
